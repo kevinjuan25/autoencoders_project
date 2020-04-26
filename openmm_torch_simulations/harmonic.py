c88d1b45106578bb67072884b66600465ddb8b80
Kevin Juan <kevinjuan25@icloud.com>
2020-04-25 21:15:23 -0400

    fixed harmoic forces and simulations

--- openmm_torch_simulations/harmonic.py
+++ openmm_torch_simulations/harmonic.py
@@ -11,16 +11,6 @@ class ForceModule(torch.nn.Module):
     def forward(self, positions):
         return torch.sum(positions ** 2)
 
-class ForceModulePBC(torch.nn.Module):
-    def forward(self, positions, boxvectors):
-        boxsize = boxvectors.diag()
-        pbc_pos = positions - torch.floor(positions / boxsize) * boxsize
-        return torch.sum(pbc_pos ** 2)
-
 harmonic = ForceModule()
-positions = torch.rand(3)
-module_no_pbc = torch.jit.trace(harmonic, positions)
-module_no_pbc.save('harmonic_no_pbc.pt')
-
-module_pbc = torch.jit.script(ForceModulePBC())
-module_pbc.save('harmonic_pbc.pt')
+module = torch.jit.script(ForceModule())
+module.save('harmonic.pt')
